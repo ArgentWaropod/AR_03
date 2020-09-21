e54c9c4140d12f0187d1dc3ab81625e66c2a10ca
ArgentWaropod <62075349+ArgentWaropod@users.noreply.github.com>
2020-09-21 10:49:33 -0400

    Upload

---
+++ AR_03/Assets/Scripts/Sliders.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sliders : MonoBehaviour
+{
+    public GameObject obj;
+    public Slider scale;
+    public Slider rotation;
+    public GameObject checkeredPlane;
+    bool isOn = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj.transform.localScale = new Vector3(scale.value, scale.value, scale.value);
+        obj.transform.rotation = new Quaternion(0, rotation.value, 0, 0);
+    }
+    public void button()
+    {
+        if (isOn)
+        {
+            checkeredPlane.SetActive(false);
+        }
+        else
+        {
+            checkeredPlane.SetActive(true);
+        }
+    }
+}
